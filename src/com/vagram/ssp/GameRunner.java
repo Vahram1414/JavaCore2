c081b471aca9a062a1a18046f3a7743aeb81756e
Vahram Alickhanyan <anton-sviridov92@mail.ru>
2023-09-23 14:01:56 +0300

    My version 23/09

--- src/com/vagram/ssp/GameRunner.java
+++ src/com/vagram/ssp/GameRunner.java
@@ -3,12 +3,12 @@ package com.vagram.ssp;
 
 public class GameRunner {
     public static void main(String[] args) {
-//        GameLogic gameLogic = new GameLogic();
-//        gameLogic.start();
-//    }
-//    Ob1 ob1 = new Ob1;
-//    Ob2 = Ob1 {
-//        System.out.println(Ob1.equals(O2));
+        GameLogic gameLogic = new GameLogic();
+        gameLogic.start();
+    }
+    Ob1 ob1 = new Ob1;
+    Ob2 = Ob1 {
+        System.out.println(Ob1.equals(O2));
     }
 
 }
