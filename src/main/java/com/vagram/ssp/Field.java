f92fa9b0c3de8400cd63dc8f9e785d8ae3707951
Vahram Alickhanyan <anton-sviridov92@mail.ru>
2023-09-23 14:02:55 +0300

    My version 23/09

---
+++ src/main/java/com/vagram/ssp/Field.java
@@ -0,0 +1,22 @@
+package com.vagram.ssp;
+
+public class Field {
+    private Figure f1;
+    private Figure f2;
+
+    public Figure getF1() {
+        return f1;
+    }
+
+    public void setF1(Figure f1) {
+        this.f1 = f1;
+    }
+
+    public Figure getF2() {
+        return f2;
+    }
+
+    public void setF2(Figure f2) {
+        this.f2 = f2;
+    }
+}
